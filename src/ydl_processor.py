65d255b648515031bb3bf051f2ef3e9e57fbfb30
vkot11 <hstudiotop@gmail.com>
2025-02-08 17:38:19 +0200

    feat: cookies interaction

--- src/ydl_processor.py
+++ src/ydl_processor.py
@@ -2,23 +2,28 @@ from functools import lru_cache
 import yt_dlp
 from song_info import SongInfo
 from playlist_info import PlaylistInfo
+import os
 
 class YdlProcessor:
     cache_size = 100
-
     def __init__(self, cache_size=100):
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        cookies_path = os.path.join(current_dir, "..", "cookies/cookies_file.txt")
+
         YDL_OPTIONS = {
             'format': 'bestaudio',
             'noplaylist': True,
             'nocheckcertificate': True,
             'no_color': True,
             'verbose': True,
+            'cookiefile': cookies_path,
         }
 
         YDL_OPTIONS_EXT = {
             'extract_flat': 'in_playlist',
             'skip_download': True,
             'quiet': True,
+            'cookiefile': cookies_path,
         }
 
         self.ydl = yt_dlp.YoutubeDL(YDL_OPTIONS)
